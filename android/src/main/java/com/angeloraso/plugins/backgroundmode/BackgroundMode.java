8ec9172675dbfb89da9d17f675e27e386e9dc0f8
Angelo Raso <araso@anura.com.ar>
2021-09-04 15:44:34 -0300

    Fix condition

--- android/src/main/java/com/angeloraso/plugins/backgroundmode/BackgroundMode.java
+++ android/src/main/java/com/angeloraso/plugins/backgroundmode/BackgroundMode.java
@@ -73,7 +73,7 @@ public class BackgroundMode {
 
     public void onPause() {
         mInBackground = true;
-        if (isEnabled()) {
+        if (!isEnabled()) {
             return;
         }
 
